577a64a60ce7d07595ac657d3214e3f943b893f1
Wei Zang <richzw@gmail.com>
2023-02-17 16:04:15 +0800

    refact(api): apply http client middleware

--- store.go
+++ store.go
@@ -97,7 +97,7 @@ func (c *StoreClient) GetALLSubscriptionStatuses(ctx context.Context, originalTr
 	var client HTTPClient
 	client = c.httpCli
 	client = SetInitializer(client, c.initHttpClient)
-	client = RequireResponseStatus(client)
+	client = RequireResponseStatus(client, http.StatusOK)
 	client = SetRequest(ctx, client, http.MethodGet, URL)
 	rsp := &StatusResponse{}
 	client = SetResponseBodyHandler(client, json.Unmarshal, rsp)
@@ -110,24 +110,23 @@ func (c *StoreClient) GetALLSubscriptionStatuses(ctx context.Context, originalTr
 }
 
 // LookupOrderID https://developer.apple.com/documentation/appstoreserverapi/look_up_order_id
-func (c *StoreClient) LookupOrderID(ctx context.Context, orderId string) (rsp *OrderLookupResponse, err error) {
+func (c *StoreClient) LookupOrderID(ctx context.Context, orderId string) (*OrderLookupResponse, error) {
 	URL := c.hostUrl + PathLookUp
 	URL = strings.Replace(URL, "{orderId}", orderId, -1)
-	statusCode, body, err := c.Do(ctx, http.MethodGet, URL, nil)
-	if err != nil {
-		return
-	}
 
-	if statusCode != http.StatusOK {
-		return nil, fmt.Errorf("appstore api: %v return status code %v", URL, statusCode)
-	}
+	var client HTTPClient
+	client = c.httpCli
+	client = SetInitializer(client, c.initHttpClient)
+	client = RequireResponseStatus(client, http.StatusOK)
+	client = SetRequest(ctx, client, http.MethodGet, URL)
+	rsp := &OrderLookupResponse{}
+	client = SetResponseBodyHandler(client, json.Unmarshal, rsp)
 
-	err = json.Unmarshal(body, &rsp)
+	_, err := client.Do(nil)
 	if err != nil {
 		return nil, err
 	}
-
-	return
+	return rsp, nil
 }
 
 // GetTransactionHistory https://developer.apple.com/documentation/appstoreserverapi/get_transaction_history
@@ -139,36 +138,33 @@ func (c *StoreClient) GetTransactionHistory(ctx context.Context, originalTransac
 		query = &url.Values{}
 	}
 
+	var client HTTPClient
+	client = c.httpCli
+	client = SetInitializer(client, c.initHttpClient)
+	client = RequireResponseStatus(client, http.StatusOK)
+
 	for {
 		rsp := HistoryResponse{}
-
-		statusCode, body, errOmit := c.Do(ctx, http.MethodGet, URL+"?"+query.Encode(), nil)
-		if errOmit != nil {
-			return nil, errOmit
-		}
-
-		if statusCode != http.StatusOK {
-			return nil, fmt.Errorf("appstore api: %v return status code %v", URL, statusCode)
-		}
-
-		err = json.Unmarshal(body, &rsp)
+		client = SetRequest(ctx, client, http.MethodGet, URL+"?"+query.Encode())
+		client = SetResponseBodyHandler(client, json.Unmarshal, rsp)
+		_, err = client.Do(nil)
 		if err != nil {
 			return nil, err
 		}
 
 		responses = append(responses, &rsp)
 		if !rsp.HasMore {
-			break
+			return
 		}
 
-		if rsp.HasMore && rsp.Revision != "" {
+		if rsp.Revision != "" {
 			query.Set("revision", rsp.Revision)
+		} else {
+			return
 		}
 
 		time.Sleep(10 * time.Millisecond)
 	}
-
-	return
 }
 
 // GetRefundHistory https://developer.apple.com/documentation/appstoreserverapi/get_refund_history
@@ -177,37 +173,35 @@ func (c *StoreClient) GetRefundHistory(ctx context.Context, originalTransactionI
 	baseURL = strings.Replace(baseURL, "{originalTransactionId}", originalTransactionId, -1)
 
 	URL := baseURL
+	var client HTTPClient
+	client = c.httpCli
+	client = SetInitializer(client, c.initHttpClient)
+	client = RequireResponseStatus(client, http.StatusOK)
+
 	for {
 		rsp := RefundLookupResponse{}
-
-		statusCode, body, errOmit := c.Do(ctx, http.MethodGet, URL, nil)
-		if errOmit != nil {
-			return nil, errOmit
-		}
-
-		if statusCode != http.StatusOK {
-			return nil, fmt.Errorf("appstore api: %v return status code %v", URL, statusCode)
-		}
-
-		err = json.Unmarshal(body, &rsp)
+		client = SetRequest(ctx, client, http.MethodGet, URL)
+		client = SetResponseBodyHandler(client, json.Unmarshal, rsp)
+		_, err = client.Do(nil)
 		if err != nil {
 			return nil, err
 		}
 
 		responses = append(responses, &rsp)
 		if !rsp.HasMore {
-			break
+			return
 		}
 
 		data := url.Values{}
-		if rsp.HasMore && rsp.Revision != "" {
+		if rsp.Revision != "" {
 			data.Set("revision", rsp.Revision)
 			URL = baseURL + "?" + data.Encode()
+		} else {
+			return
 		}
 
 		time.Sleep(10 * time.Millisecond)
 	}
-	return
 }
 
 // SendConsumptionInfo https://developer.apple.com/documentation/appstoreserverapi/send_consumption_information
@@ -247,7 +241,6 @@ func (c *StoreClient) ExtendSubscriptionRenewalDate(ctx context.Context, origina
 }
 
 // GetNotificationHistory https://developer.apple.com/documentation/appstoreserverapi/get_notification_history
-// Note: Notification history is available starting on June 6, 2022. Use a startDate of June 6, 2022 or later in your request.
 func (c *StoreClient) GetNotificationHistory(ctx context.Context, body NotificationHistoryRequest) (responses []NotificationHistoryResponseItem, err error) {
 	baseURL := c.hostUrl + PathGetNotificationHistory
 
@@ -258,39 +251,38 @@ func (c *StoreClient) GetNotificationHistory(ctx context.Context, body Notificat
 	}
 
 	URL := baseURL
+	var client HTTPClient
+	client = c.httpCli
+	client = SetInitializer(client, c.initHttpClient)
+	client = RequireResponseStatus(client, http.StatusOK)
+
 	for {
 		rsp := NotificationHistoryResponses{}
 		rsp.NotificationHistory = make([]NotificationHistoryResponseItem, 0)
 
-		statusCode, rspBody, errOmit := c.Do(ctx, http.MethodPost, URL, bodyBuf)
-		if errOmit != nil {
-			return nil, errOmit
-		}
-
-		if statusCode != http.StatusOK {
-			return nil, fmt.Errorf("appstore api: %v return status code %v", URL, statusCode)
-		}
-
-		err = json.Unmarshal(rspBody, &rsp)
+		client = SetRequest(ctx, client, http.MethodPost, URL)
+		client = SetRequestBodyJSON(client, bodyBuf)
+		client = SetResponseBodyHandler(client, json.Unmarshal, rsp)
+		_, err = client.Do(nil)
 		if err != nil {
 			return nil, err
 		}
 
 		responses = append(responses, rsp.NotificationHistory...)
 		if !rsp.HasMore {
-			break
+			return responses, nil
 		}
 
 		data := url.Values{}
-		if rsp.HasMore && rsp.PaginationToken != "" {
+		if rsp.PaginationToken != "" {
 			data.Set("paginationToken", rsp.PaginationToken)
 			URL = baseURL + "?" + data.Encode()
+		} else {
+			return responses, nil
 		}
 
 		time.Sleep(10 * time.Millisecond)
 	}
-
-	return responses, nil
 }
 
 // SendRequestTestNotification https://developer.apple.com/documentation/appstoreserverapi/request_a_test_notification
@@ -347,25 +339,6 @@ func (c *StoreClient) parseSignedTransaction(transaction string) (*JWSTransactio
 	return tran, nil
 }
 
-func (c *StoreClient) doRequest(ctx context.Context, client HTTPClient, method, url string, body io.Reader) (int, []byte, error) {
-	req, err := http.NewRequestWithContext(ctx, method, url, body)
-	if err != nil {
-		return 0, nil, err
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return 0, nil, err
-	}
-	defer resp.Body.Close()
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return 0, nil, fmt.Errorf("appstore read http body err %w", err)
-	}
-	return resp.StatusCode, bodyBytes, nil
-}
-
 // Per doc: https://developer.apple.com/documentation/appstoreserverapi#topics
 func (c *StoreClient) Do(ctx context.Context, method string, url string, body io.Reader) (int, []byte, error) {
 	authToken, err := c.Token.GenerateIfExpired()
@@ -389,10 +362,10 @@ func (c *StoreClient) Do(ctx context.Context, method string, url string, body io
 	}
 	defer resp.Body.Close()
 
-	bytes, err := io.ReadAll(resp.Body)
+	byteData, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return resp.StatusCode, nil, fmt.Errorf("appstore read http body err %w", err)
 	}
 
-	return resp.StatusCode, bytes, err
+	return resp.StatusCode, byteData, err
 }
